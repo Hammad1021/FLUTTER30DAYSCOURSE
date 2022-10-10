e160844d759d18f8ef1f26aa1dcc010bb2ba2bdc
Hammad1021 <102464925+Hammad1021@users.noreply.github.com>
2022-10-10 13:40:30 +0100

    day_10

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:day_4/home_page.dart';
 import 'package:day_4/utils/route.dart';
+import 'package:day_4/widget/themes.dart';
 import 'login_page.dart';
 import 'package:flutter/material.dart';
 import 'package:material_color_utilities/material_color_utilities.dart';
@@ -18,16 +19,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         themeMode: ThemeMode.light,
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-          elevatedButtonTheme: ElevatedButtonThemeData(
-            style: ElevatedButton.styleFrom(
-                primary: Colors.amber, // background (button) color
-                onPrimary: Colors.black,
-                minimumSize: Size(100, 35) // foreground (text) color
-                ),
-          ),
-        ),
+        theme: MyTheme.LightTheme(context),
         initialRoute: MyRoutes.HomePageroute,
         routes: {
           MyRoutes.HomePageroute: (context) => homepage(),
---
+++ lib/widget/themes.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+import 'package:day_4/widget/themes.dart';
+import 'package:flutter/material.dart';
+
+class MyTheme {
+  static ThemeData LightTheme(BuildContext context) => ThemeData(
+        appBarTheme: AppBarTheme(
+            color: Colors.white,
+            elevation: 0.0,
+            iconTheme: IconThemeData(color: Colors.black)),
+        textTheme: Theme.of(context).textTheme,
+        primarySwatch: Colors.blue,
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+              primary: Colors.amber, // background (button) color
+              onPrimary: Colors.black,
+              minimumSize: Size(100, 35)
+              // foreground (text) color
+              ),
+        ),
+      );
+}
