3411173299f7a8ecd17034904183c214f9fc484a
Hammad1021 <102464925+Hammad1021@users.noreply.github.com>
2022-10-11 14:39:43 +0100

    day_14

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/material.dart';
+import 'package:flutter/src/material/colors.dart';
+import 'package:material_color_utilities/material_color_utilities.dart';
+
 import 'package:day_4/home_page.dart';
 import 'package:day_4/utils/route.dart';
 import 'package:day_4/widget/themes.dart';
+
 import 'login_page.dart';
-import 'package:flutter/material.dart';
-import 'package:material_color_utilities/material_color_utilities.dart';
-import 'package:flutter/src/material/colors.dart';
 
 void main() {
   runApp(const MyApp());
