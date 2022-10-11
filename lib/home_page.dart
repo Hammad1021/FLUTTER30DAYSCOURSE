6f970bf77d1369267216e1f4a301c30fcf1e7a43
Hammad1021 <102464925+Hammad1021@users.noreply.github.com>
2022-10-11 12:37:37 +0100

    day_`13

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,17 +1,42 @@
+import 'dart:convert';
+
 import 'package:day_4/model/catalog.dart';
 import 'package:day_4/model/catalog.dart';
 
 import 'package:day_4/widget/Drawer.dart';
 import 'package:flutter/material.dart';
 import 'package:day_4/widget/Drawer.dart';
+import 'package:flutter/services.dart';
 
 import 'model/catalog.dart';
 import 'widget/item_widget.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
 
-class homepage extends StatelessWidget {
+class homepage extends StatefulWidget {
   const homepage({super.key});
+
+  @override
+  State<homepage> createState() => _homepageState();
+}
+
+class _homepageState extends State<homepage> {
   final int days = 30;
+
   final String name = 'HAMMAD';
+  @override
+  void initState() {
+    super.initState();
+    LoadData();
+  }
+
+  LoadData() async {
+    final CatalogJson =
+        await rootBundle.loadString("assets/files/catalog.json");
+    final decodedData = jsonDecode(CatalogJson);
+    var Productdata = decodedData["products"];
+  }
+
   @override
   Widget build(BuildContext context) {
     final dummylist = List.generate(20, (index) => CatalogModel.items[0]);
